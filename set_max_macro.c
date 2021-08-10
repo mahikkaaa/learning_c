499771851ecb5705d8eb5c98de7c784a532e7902
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-10 13:04:15 +0530

    Add files via upload

---
+++ set_max_macro.c
@@ -0,0 +1,12 @@
+//Defining constant in c
+
+#include<stdio.h>
+#define max 100 //macro
+
+main()
+{
+   const float pi=3.14;
+    printf("\n Value of pi is %f",pi);
+
+    printf("\n Final exam paper  is out of %d",max);
+}
