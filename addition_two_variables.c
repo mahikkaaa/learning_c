8a0cf3af5da89078177432ab6acfc7a39b935d06
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-10 09:17:04 +0530

    Add files via upload

---
+++ addition_two_variables.c
@@ -0,0 +1,14 @@
+//program to add two variables
+#include<stdio.h>
+main()
+{
+	int num1, num2, num3;
+
+	printf("enter first number : ");
+	scanf("%d", num1);
+	printf("enter second number : ");
+	scanf("%d", num2);
+
+	num3=num1+num2;
+	printf("sum of two numbers is %d", num3);
+}
