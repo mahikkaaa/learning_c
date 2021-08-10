cb614a4ac38f2584857825f241304c84bfb5e76e
mahikkaaa <chandrikaagrawal123@gmail.com>
2021-08-10 09:07:43 +0530

    first program

---
+++ print_variable.c
@@ -0,0 +1,17 @@
+//program to print variables 
+#include<stdio.h>
+main()
+{
+	int rollno=14;
+	char grade ='A';
+	float marks=99.9;
+	char name[50]="Mahika"
+	char username[50]="mahikkaaa"
+
+
+	printf("Roll number is %d",rollno);
+    printf("\n Grade is %c",grade);
+    printf("\n Score is %f",marks);
+    printf("\n Name is %s",name);
+    printf("\n username is %s", username);
+}
