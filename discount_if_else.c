b85878490c1ab1c8fad7d7a274950a186f6bb2c9
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-10 16:27:57 +0530

    Add files via upload

---
+++ discount_if_else.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+    float purchase_amount,discount,amount_to_be_paid;
+
+    printf("enter purchase amount ");
+    scanf("%f",&purchase_amount);
+
+    if(purchase_amount>=5000)
+    {
+        discount=purchase_amount*0.10;
+        amount_to_be_paid=purchase_amount-discount;
+
+        printf("\n You get discount of Rs %f ",discount);
+        printf("\nAmount to be paid by customer is %f\n\n",amount_to_be_paid);
+    }
+    else
+    {
+        discount=0;
+        amount_to_be_paid=purchase_amount-discount;
+        printf("\n You get discount of Rs %f ",discount);
+        printf("\nAmount to be paid by customer is %f\n\n",amount_to_be_paid);
+    }
+}
