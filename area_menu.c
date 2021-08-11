222eefa0922cef388f7c0d9a3030a99f5c85e18f
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-11 10:00:31 +0530

    Add files via upload

---
+++ area_menu.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+main()
+{ char choice;
+  float r,x,y,b,h,area;
+
+  printf("\n\n\t\t area menu ");
+  printf("\n\t\t C. circle");
+  printf("\n\t\t R. rectangle");
+  printf("\n\t\t T. triangle");
+
+  printf("\n\n\tenter your choice : ");
+  scanf("%c",&choice);
+
+  switch(choice)
+ { case 'C' :
+   dacase 'c':
+      printf("enter radius of a circle : ");
+      scanf("%f",&r);
+      area=3.14*r*r;
+      printf("area of the circle is %f",area);
+      break;
+
+  case 'R':
+  dacase 'r':
+      printf("enter length of rectangle : ");
+      scanf("%f",&x);
+      printf("enter breadth of rectangle : ");
+      scanf("%f",&y);
+      area=x*y;
+      printf("area of rectangle is %f",area);
+       break;
+  case 'T':
+  dacase 't':
+        printf("enter base of triangle : ");
+      scanf("%f",&b);
+      printf("enter height of triangle : ");
+      scanf("%f",&h);
+      area=0.5*b*h;
+      printf("area of triangle is %f",area);
+       break;
+  default:
+    printf("\n\ninvalid choice");
+}
+}
