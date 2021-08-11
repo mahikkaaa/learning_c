eab519d8c62434f70ea03fc659a467507c8af861
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-11 14:44:51 +0530

    Add files via upload

---
+++ Fibonacci_series.c
@@ -0,0 +1,72 @@
+//to print Fibonacci series
+#include<stdio.h>
+main()
+{
+   int num=0;
+   int fn=0,sn=1,tn=0,i=0;
+
+   printf("enter number you want to display in Fibonacci series ");
+   scanf("%d",&num);
+
+   printf("%d  %d",fn,sn);
+   //printing series
+   for(i=3;i<=num;i++)
+   {   tn=fn+sn;
+       printf("  %d",tn);
+       fn=sn;
+       sn=tn;
+   }
+}
+
+/**
+MODIFICATION 
+#include<stdio.h>
+main()
+{
+    int num,i=3;
+    int firstnumber=0;
+    int secondnumber=1;
+    int thirdnumber;
+
+    printf("Enter number upto which you want to print fiboncci number ");
+    scanf("%d",&num);
+
+    printf("\n %d\t%d",firstnumber,secondnumber);
+
+    while(i<=num)   //for(i=3;i<=num;i++)
+    {
+        thirdnumber=firstnumber+secondnumber;
+        printf("\t%d",thirdnumber);
+        firstnumber=secondnumber;
+        secondnumber=thirdnumber;
+        i++;
+    }
+
+
+}
+
+
+
+
+.
+#include<stdio.h>
+main()
+{
+    int i,n,first=0,second=1,third;
+
+    printf("how many numbers you want to display in Fibonacci series ");
+    scanf("%d",&n);
+
+    printf("%d  %d ",first,second);
+
+    for(i=0;i<=n;i++)
+    {
+        third=first+second;
+        printf("%d ",third);
+        first=second;
+        second=third;
+    }
+}
+
+
+**/
