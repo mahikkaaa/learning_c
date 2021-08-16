34311a7fb2900251aa979b3b1443e2ef342bf053
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-16 22:16:46 +0530

    Update Fibonacci_series.c

--- Fibonacci_series.c
+++ Fibonacci_series.c
@@ -70,3 +70,39 @@ main()
 
 
 **/
+
+
+
+/**
+FIBONACCI WITH RECURSION 
+//Let's see an example to find the nth term of the Fibonacci series.
+//0 1 1 2 3 5 8 ...
+ #include<stdio.h>
+int fibonacci(int);
+void main ()
+{
+  int n,f;
+  for(n=0;n<=10;n++)
+  {f = fibonacci(n);
+    printf("  %d",f);
+  }
+}
+
+int fibonacci (int n)
+{
+ if (n==0)
+ {
+  return 0;
+ }
+ else if (n == 1)
+ {
+ return 1;
+ }
+ else
+{
+ return fibonacci(n-1)+fibonacci(n-2);
+ }
+}
+
+
+**/
