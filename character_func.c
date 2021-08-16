fe9dcfd8c5cb156b9d9dce18c19784bd74a22791
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-16 22:13:41 +0530

    Update character_func.c

--- character_func.c
+++ character_func.c
@@ -10,3 +10,43 @@ main()
  printf("\n%c",tolower(ch));
  printf("\n%d",ch);
 }
+
+
+/**
+MODIFICATION
+
+#include<stdio.h>
+#include<ctype.h>
+main()
+{
+    char ch;
+    printf("enter a single character ");
+    ch=getchar();
+
+    if(isalpha(ch))
+    {
+        if(isupper(ch))
+       {
+         ch=tolower(ch);
+         printf("\n Ch is %c",ch);
+
+       }
+       else if(islower(ch))
+       {
+           ch=toupper(ch);
+           printf("\n Ch is %c",ch);
+
+       }
+    }
+  else if(isdigit(ch))
+    {
+        printf("It is a digit");
+    }
+  else
+    {
+
+        printf("\n It must be special character");
+    }
+
+}
+**/
